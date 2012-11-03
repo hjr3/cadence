a66fd4dc18a8e1940ffe77bf1d11578247c66079
Herman J. Radtke III <hermanradtke@gmail.com>
2012-11-02 18:42:48 -0700

    Initial commit of Cadence.

---
+++ README.md
@@ -0,0 +1,67 @@
+# Cadence #
+The _easy_ way to record time in Jira / Tempo.
+
+## Instatllation ##
+1. ```git clone https://github.com/hradtke/cadence.git```
+1. ```cp cadence/.jira.dist ~/```
+1. ```vim ~/.jira``` and fill in the configuration values
+
+## Log Format ##
+The log format is very simple so work can quickly be recorded. Each entry in the work log must be on a separate line. Blank lines are permitted.
+
+Syntax for work log entries:
+
+    Entry       = issue time-format comment
+    issue       = [A-Z]([A-Z0-9_]+)-[1-9][0-9]*
+    time-format = [1-9][0-9]*(d|h|m)
+    comment     = *
+
+Example:
+
+    TASK-130 15m scrum
+    PROJECT-52 3h worked on the widget
+    HR-11 30m performed me an interview
+
+## Sending the work log to Jira ##
+Cadence will display each issue it logs work to.
+
+Example:
+
+    # php ~/cadence/bin/cadence.php
+    DISC-130
+    DELTA-52
+    WAP-411
+    API-43
+    DISC-89
+    DISC-25
+    API-363
+    IOS-1698
+    API-43
+    API-43
+    ATASKS-3
+    ATASKS-4
+    DISC-172
+    DISC-89
+    ATASKS-3
+
+If there is a problem logging the work, Cadence will show the error detail. It will continue logging subsequent entries in the work log.
+
+Example
+
+    # php bin/cadence.php 
+    Failed to record time for BOGUS-100
+    HTTP Response code of 404
+    stdClass Object
+    (
+        [errorMessages] => Array
+            (
+                [0] => Issue Does Not Exist
+            )
+
+        [errors] => stdClass Object
+            (
+            )
+    
+    )
+
+
---
+++ bin/cadence.php
@@ -0,0 +1,82 @@
+<?php
+
+// special awesome Jira date format
+$now = date('Y-m-d\TH:i:s.000O');
+
+$iniFile = $_SERVER['HOME'] . '/.jira';
+
+if (!file_exists($iniFile)) {
+    echo "Unable to locate configuration file: {$iniFile}", PHP_EOL;
+}
+
+$info = parse_ini_file($iniFile);
+$host = $info['host'];
+$logFile = $info['logFile'];
+$username = $info['username'];
+$password = $info['password'];
+
+if (!file_exists($logFile)) {
+    echo "Unable to locate work log: {$logFile}", PHP_EOL;
+}
+
+$workLog = file_get_contents($logFile);
+
+$tasks = explode(PHP_EOL, $workLog);
+
+foreach ($tasks as $task) {
+    $parts = explode(' ', $task, 3);
+
+    // skip empty lines
+    if (!$parts[0]) {
+        continue;
+    }
+
+    list($issue, $timeSpent, $comment) = $parts;
+
+    $data = array(
+        'timeSpent' => $timeSpent,
+        'started' => $now,
+        'comment' => $comment,
+    );
+
+    logMyWork($issue, $data);
+}
+
+function logMyWork($issue, $data)
+{
+    global $host;
+    global $username;
+    global $password;
+
+    $url = "http://{$host}/rest/api/2/issue/{$issue}/worklog";
+    $json = json_encode($data);
+    $headers = array(
+        'Accept: application/json',
+        'Content-Type: application/json',
+    );
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_VERBOSE, false);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_USERPWD, "{$username}:{$password}");
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    
+    $output = curl_exec($ch);
+    $httpStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    curl_close($ch);
+
+    if ($httpStatus !== 201) {
+        echo "Failed to record time for {$issue}", PHP_EOL;
+        echo 'HTTP Response code of ' , $httpStatus, PHP_EOL;
+        print_r(json_decode($output));
+        return false;
+    }
+
+    echo $issue, PHP_EOL;
+    
+    return true;
+}
